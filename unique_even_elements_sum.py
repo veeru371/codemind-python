f10fa7c4c34ad861ca6ad3c26c0d762bb6f20a1f
veeru371 <84440176+veeru371@users.noreply.github.com>
2022-05-04 15:42:43 +0530

    N=int(input()) s=0 k=list(map(int,input().split())) t=[] for i in k:     if i not in t:         t.append(i)
    for j in range(len(t)):
        if t[j]%2==0:
            s=s+t[j]
    print(s)

---
+++ unique_even_elements_sum.py
@@ -0,0 +1,12 @@
+N=int(input())
+s=0
+k=list(map(int,input().split()))
+t=[]
+for i in k:
+    if i not in t:
+        t.append(i)
+        
+for j in range(len(t)):
+    if t[j]%2==0:
+        s=s+t[j]
+print(s)
